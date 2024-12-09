41c9137651dd25b9fd88c6fa343c1449c6a41e46
Hiba <hibakaawan98@gmail.com>
2024-12-09 01:58:45 -0500

    api connected to comments

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -1,13 +1,24 @@
 import './CommentForm.scss';
+import axios from 'axios';
+import { useParams } from "react-router-dom";
 function CommentForm(){
+    const apiKey = "bb1afd72-69af-4298-bc35-5df91725a3f2";
+    const baseURL = "https://unit-3-project-c5faaab51857.herokuapp.com/photos";
+    const param = useParams();
+
     async function handleSubmit(event){
         event.preventDefault();
         const newComment = {
             name: event.target.name.value,
             comment: event.target.comment.value
         };
+        try{
+            const response = await axios.post(`${baseURL}/${param.photoId}/comments?api_key=${apiKey}`, newComment)
+            console.log(response.data);
+        }catch (error) {
+            console.log("Error posting new comment", error);
+        }
         
-        console.log(newComment);
     }
     return (
         <div className='form-container'>
